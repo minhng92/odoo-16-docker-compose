7c11d281bb1023ba7316d5a2e128c6d0be2086fc
minhng92 <minhng92@gmail.com>
2024-11-03 17:23:56 +0700

    update permissions and for macos

--- run.sh
+++ run.sh
@@ -2,18 +2,48 @@
 DESTINATION=$1
 PORT=$2
 CHAT=$3
-# clone Odoo directory
+
+# Clone Odoo directory
 git clone --depth=1 https://github.com/minhng92/odoo-16-docker-compose $DESTINATION
 rm -rf $DESTINATION/.git
-# set permission
+
+# Create PostgreSQL directory
 mkdir -p $DESTINATION/postgresql
-sudo chmod -R 777 $DESTINATION
-# config
-if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
-sudo sysctl -p
-sed -i 's/10016/'$PORT'/g' $DESTINATION/docker-compose.yml
-sed -i 's/20016/'$CHAT'/g' $DESTINATION/docker-compose.yml
-# run Odoo
+
+# Change ownership to current user and set restrictive permissions for security
+sudo chown -R $USER:$USER $DESTINATION
+sudo chmod -R 700 $DESTINATION  # Only the user has access
+
+# Check if running on macOS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "Running on macOS. Skipping inotify configuration."
+else
+  # System configuration
+  if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then
+    echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf)
+  else
+    echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf
+  fi
+  sudo sysctl -p
+fi
+
+# Set ports in docker-compose.yml
+# Update docker-compose configuration
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # macOS sed syntax
+  sed -i '' 's/10016/'$PORT'/g' $DESTINATION/docker-compose.yml
+  sed -i '' 's/20016/'$CHAT'/g' $DESTINATION/docker-compose.yml
+else
+  # Linux sed syntax
+  sed -i 's/10016/'$PORT'/g' $DESTINATION/docker-compose.yml
+  sed -i 's/20016/'$CHAT'/g' $DESTINATION/docker-compose.yml
+fi
+
+# Set file and directory permissions after installation
+find $DESTINATION -type f -exec chmod 644 {} \;
+find $DESTINATION -type d -exec chmod 755 {} \;
+
+# Run Odoo
 docker-compose -f $DESTINATION/docker-compose.yml up -d
 
-echo 'Started Odoo @ http://localhost:'$PORT' | Master Password: minhng.info | Live chat port: '$CHAT
+echo "Odoo started at http://localhost:$PORT | Master Password: minhng.info | Live chat port: $CHAT"
